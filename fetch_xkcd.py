3ab6b36771b8a2483b48e1f54b3c43cd5f32bde7
Yulia51188 <sun51188@gmail.com>
2019-03-08 14:04:55 +0300

    Update fetch_xkcd.py
    Make function to get response from site

--- fetch_xkcd.py
+++ fetch_xkcd.py
@@ -1,10 +1,27 @@
 import download_images
+import requests
+
+def fetch_xkcd(host='http://xkcd.com', method='info.0.json', number=None):
+    method_api = method
+    if not number is None:
+        method_api = '{0}/{1}'.format(number, method)
+    url = '{host}/{method}'.format(host=host, method=method_api)
+    response = requests.get(url)
+    response.raise_for_status()
+    if response.ok:
+        return response.json()
+
 
 
 def main():
-    xkcd_url = 'http://xkcd.com/{number}/info.0.json'.format(number=353)
-    comics_url = 'https://imgs.xkcd.com/comics/python.png'
-    print(download_images.download_image(comics_url, 'python_comics.png'))
+    current_comics = fetch_xkcd()
+    current_comics_url = current_comics['img']
+    print(current_comics_url)
+    
+    python_comics = fetch_xkcd(number=353)
+    python_comics_url = python_comics['img']
+    print(python_comics_url)
+    #print(download_images.download_image(comics_url, 'python_comics.png'))
 
 
 if __name__ == '__main__':
