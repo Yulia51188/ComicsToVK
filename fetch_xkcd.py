ced46b1940205cb37d75a2c5331d46bc252a1379
Yulia51188 <sun51188@gmail.com>
2019-03-14 10:46:14 +0300

    Add  def fetch_random_comics():

--- fetch_xkcd.py
+++ fetch_xkcd.py
@@ -1,5 +1,7 @@
 import download_images
 import requests
+import random
+
 
 def fetch_xkcd(host='http://xkcd.com', method='info.0.json', number=None):
     method_api = method
@@ -12,16 +14,32 @@ def fetch_xkcd(host='http://xkcd.com', method='info.0.json', number=None):
         return response.json()
 
 
+def fetch_random_comics():
+    max_number = get_comics_count()
+    random.seed()
+    number = random.randint(1, max_number)
+    print('Number {}'.format(number))
+
+
+
+
+
+def get_comics_count():
+    response = fetch_xkcd()
+    return response['num']
+
 
 def main():
-    current_comics = fetch_xkcd()
-    current_comics_url = current_comics['img']
-    print(current_comics_url)
+    fetch_random_comics()
+    # current_comics = fetch_xkcd()
+    # current_comics_url = current_comics['img']
+    # print(current_comics_url)
     
-    python_comics = fetch_xkcd(number=353)
-    python_comics_url = python_comics['img']
-    print(python_comics_url)
+    # python_comics = fetch_xkcd(number=353)
+    # python_comics_url = python_comics['img']
+    # print(python_comics_url)
     #print(download_images.download_image(comics_url, 'python_comics.png'))
+    
 
 
 if __name__ == '__main__':
