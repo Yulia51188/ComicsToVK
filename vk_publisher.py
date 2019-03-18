ccf3cb4649b88a72a121a0e7b8e4699474eec3ef
Yulia51188 <sun51188@gmail.com>
2019-03-18 11:31:59 +0300

    Add def get_server_upload_url and def upload_image_to_server()

--- vk_publisher.py
+++ vk_publisher.py
@@ -25,35 +25,45 @@ def make_post_request_to_vk(url=None, method=None, params={}, files = {}, host='
         return response.json()
 
 
-def main():
-    load_dotenv()
-    access_token = os.getenv("ACCESS_TOKEN")
-    vk_api_version = os.getenv("VERSION")
-    vk_group_id = os.getenv("GROUP_ID")
-    
-    comics_photo = fetch_xkcd.download_random_comics()
+def get_server_upload_url(payload):
+    response_getwall = get_request_to_vk('photos.getWallUploadServer', payload=payload)
+    print('Ключи getwall: {}'.format(response_getwall.keys()))
+    if (not 'response' in response_getwall.keys() or 
+            not 'upload_url' in response_getwall['response'].keys()):
+        return
+    return response_getwall['response']['upload_url']
 
-    payload = {
+
+def upload_image_to_server(filename, upload_url):
+    image_file_descriptor = open(filename, 'rb')
+    post_data = { 'photo': image_file_descriptor}
+    response_upload = make_post_request_to_vk(url=upload_url, files=post_data)
+    if (not 'photo' in response_upload.keys() or 
+        response_upload['photo'] is []):
+        return
+    return response_upload
+
+
+def publish_comics_to_wall(access_token, vk_group_id, version, file_data):
+    base_payload = {
         'access_token': access_token,
-        'v': vk_api_version,  
+        'v': version,  
         'group_id': vk_group_id, 
     }
-    #Получение адреса сервера
-    response_getwall = get_response_from_vk('photos.getWallUploadServer', payload=payload)
-    #
-    upload_url = response_getwall['response']['upload_url']
+    upload_url = get_server_upload_url(base_payload)
+    if upload_url is None:
+        exit('Get server upload url failed')  
     #Формируем структуру данных с изображением
-    image_file_descriptor = open(comics_photo['filename'], 'rb')
-    post_data = { 'photo': image_file_descriptor}
-    #Загружаем изображение на сервер
-    response_upload_photo = make_post_request_to_vk(url=upload_url, files=post_data)
+    image_server_data = upload_image_to_server(file_data['filename'], upload_url)
+    if image_server_data is None:
+        exit('Upload image to server failed')        
     #Разбираем данные из ответа сервера
-    photo_server = response_upload_photo['server']
-    photo_json = response_upload_photo['photo']
-    photo_hash = response_upload_photo['hash']
+    photo_server = image_server_data['server']
+    photo_json = image_server_data['photo']
+    photo_hash = image_server_data['hash']
     payload = {
         'access_token': access_token,
-        'v': vk_api_version,  
+        'v': version,  
         'server': photo_server,
         'photo': photo_json,
         'hash': photo_hash, 
@@ -70,18 +80,19 @@ def main():
     attachments_temp = 'photo{owner_id}_{media_id}'
     payload = {
         'access_token': access_token,
-        'v': vk_api_version,  
+        'v': version,  
         'owner_id': '-{id}'.format(id=vk_group_id),
         'attachments': attachments_temp.format(owner_id=owner_id, 
                                                 media_id=media_id),
         'from_group': 1,
-        'message': comics_photo['alt']
+        'message': file_data['alt']
     }
     
     #url = '{host}/{method}'.format(host='https://api.vk.com/method', 
     #                                method='wall.post')
     response_wallpost = make_post_request_to_vk(method='wall.post', params=payload)
     print(response_wallpost)
+    return True
 
 
 def main():
