1077f0c1425139b069490ccd99bd9c521bdc66ad
Yulia51188 <sun51188@gmail.com>
2019-03-18 17:50:43 +0300

    Format long strings

--- vk_publisher.py
+++ vk_publisher.py
@@ -6,7 +6,7 @@ from download_images import delete_file_and_dir
 
 
 def get_request_to_vk(method, payload={}, 
-                        host='https://api.vk.com/method'):
+        host='https://api.vk.com/method'):
     url = '{host}/{method}'.format(host=host, method=method)
     print('GET request to {url}'.format(url=url))
     response = requests.get(url, params=payload)
@@ -15,7 +15,8 @@ def get_request_to_vk(method, payload={},
         return response.json()
 
 
-def make_post_request_to_vk(url=None, method=None, params={}, files = {}, host='https://api.vk.com/method'):
+def make_post_request_to_vk(url=None, method=None, params={}, 
+        files = {}, host='https://api.vk.com/method'):
     if url is None:
         url = '{host}/{method}'.format(host=host, method=method)
     print('POST request to {url}'.format(url=url))
@@ -26,7 +27,10 @@ def make_post_request_to_vk(url=None, method=None, params={}, files = {}, host='
 
 
 def get_server_upload_url(payload):
-    response_getwall = get_request_to_vk('photos.getWallUploadServer', payload=payload)
+    response_getwall = get_request_to_vk(
+        'photos.getWallUploadServer', 
+        payload=payload
+    )
     print('Ключи getwall: {}'.format(response_getwall.keys()))
     if (not 'response' in response_getwall.keys() or 
             not 'upload_url' in response_getwall['response'].keys()):
@@ -68,12 +72,17 @@ def post_image_to_wall(image_album_data, base_payload, comment=''):
     payload = base_payload
     payload['owner_id'] = '-{id}'.format(id=base_payload['group_id'])
     attachments_temp = 'photo{owner_id}_{media_id}'
-    attachments = attachments_temp.format(owner_id=owner_id, media_id=media_id)    
+    attachments = attachments_temp.format(
+        owner_id=owner_id, 
+        media_id=
+    )    
     payload['attachments'] = attachments
     payload['from_group'] = 1
     payload['message'] = comment
-    response_wallpost = make_post_request_to_vk(method='wall.post', 
-                                                params=payload)    
+    response_wallpost = make_post_request_to_vk(
+        method='wall.post', 
+        params=payload
+    )    
     if (not 'response' in response_wallpost.keys() or
             not 'post_id' in response_wallpost['response'].keys()):
         return
@@ -89,16 +98,21 @@ def post_photo_to_wall(access_token, vk_group_id, version, file_data):
     upload_url = get_server_upload_url(base_payload)
     if upload_url is None:
         exit('Get server upload url failed')  
-    image_server_data = upload_image_to_server(file_data['filename'], upload_url)
+    image_server_data = upload_image_to_server(
+        file_data['filename'], 
+        upload_url
+    )
     if image_server_data is None:
         exit('Upload image to server failed')        
     image_album_data = add_image_to_album(image_server_data, base_payload)
     if image_album_data is None:
         exit('Adding image to server failed')      
-    return post_image_to_wall(image_album_data, base_payload, comment=file_data['alt'])
-
-
-
+    post_id = post_image_to_wall(
+        image_album_data, 
+        base_payload, 
+        comment=file_data['alt']
+    )
+    return post_id
 
 def main():
     load_dotenv()
@@ -114,17 +128,15 @@ def main():
     )
     if post_id is None:
         exit("The comics can't be posted to the wall")
-    print('The random comics #{number} is posted to the wall of group \
-        {group_id} with post id {post_id}'.format(
+    print('The random comics #{number} is posted to the wall of group'
+        '{group_id} with post id {post_id}'.format(
             number=comics_photo['num'],
             group_id=vk_group_id,
             post_id=post_id
         ))
     for item in delete_file_and_dir(comics_photo['filename'])['msg']:
         print(item)
-    exit()
-
-    
+    exit() 
     
 
 if __name__ == '__main__':
